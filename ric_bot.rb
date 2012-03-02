d9bb0bb924fc93f727ad92a242529c1981adcec4
rice-american <kazuto.agena@gmial.com>
2012-03-03 01:45:04 +0900

    ric_bot.rb README update3

--- ric_bot.rb
+++ ric_bot.rb
@@ -5,28 +5,27 @@ require 'twitter'
 require 'pp'
 
 Twitter.configure do |config|
-		config.consumer_key= "beIoy3ZKwxwpaRkjsTapsw"
-		config.consumer_secret= "vK2IwokB0gtc1B5UqXGvS7HDlpa2xDJ3d3Dtg4CGzs"
-		config.oauth_token="498604779-cDkCMkW2fdV89Mf4SfaECJF2AKZrLrjj4n8bnZ2k"
-		config.oauth_token_secret="ZoRVUBGka8YLVup3LTTYeJ5MBx6U0GHSS40lDCQ5io"
+	config.consumer_key= "beIoy3ZKwxwpaRkjsTapsw"
+	config.consumer_secret= "vK2IwokB0gtc1B5UqXGvS7HDlpa2xDJ3d3Dtg4CGzs"
+	config.oauth_token="498604779-cDkCMkW2fdV89Mf4SfaECJF2AKZrLrjj4n8bnZ2k"
+	config.oauth_token_secret="ZoRVUBGka8YLVup3LTTYeJ5MBx6U0GHSS40lDCQ5io"
 end
 
 num=10
 
 #botがつぶやく言葉が格納される配列
-words = Array.new(num)
+words = Array.new
 #ダイレクトメッセージのテキストが格納される配列
-dmArray = Array.new(num-1,nil)
+dmArray = Array.new
 dmInfo=Twitter.direct_messages(count:num)
 
 dmInfo.each do |dm|
-		#dmArray先頭にdm.textを格納していく
-		dmArray.unshift(dm.text)
-		puts dm.text
+	#dmArray先頭にdm.textを格納していく
+	dmArray.unshift(dm.text)
+	#puts dm.text
 end
 
 dmArray.each do |da|
-
 #DMtextを(半角OR全角)スペースまたは,ごとに分割し、各変数に格納
 sub = da.split(/\s|　|,/)[0]
 dl = da.split(/\s|　|,/)[1]
@@ -47,23 +46,25 @@ dlday = Time.local(2012,d_month.to_i,d_date.to_i,d_hour.to_i,d_minute.to_i)
 nowday = Time.new
 
 #時間の差
+#dlday < nowdayだった場合、daysは負になる
 days = (dlday - nowday).divmod(24*60*60)
 hours = days[1].divmod(60*60) 
 mins = hours[1].divmod(60) 
 
-end
-#残り時間整形
-deadline = "#{days[0].to_i} 日 #{hours[0].to_i} 時間 #{mins[0].to_i} 分 " 
+#もし締切りが過ぎていなかったら
+	if (days[0].to_i >= 0) then
+	#残り時間整形
+	deadline = "#{days[0].to_i} 日 #{hours[0].to_i} 時間 #{mins[0].to_i} 分 " 
 
-#実際にbotがつぶやく文章を整形
-		dmArray.unshift(dm.text)
+#wordsに整形した文字列を格納
 words.unshift("教科名: " + sub + "     締切日: " + d_month + "月" + d_date + "日" + "(" + week + ")" +
 "    内容: " + con + "     締切まであと" + deadline.to_s + "です。") 
+end
 
-#Wordsに格納された配列を呟く
-words.echo do |word|
-	if (days[0] <= 0) then
-	puts word
-	#Twitter.update(word)
-	end
+#wordsの中の要素をつぶやく
+words.each do |word|
+	Twitter.update(word)
+end
+
+
 end
